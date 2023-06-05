2f3a031f781c7ec1d5f5dbbe4b6c53c8b934c2af
saurabhlm10 <saurabhlm10@gmail.com>
2023-06-05 12:50:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {
+  DndContext,
+  closestCenter
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy
+} from "@dnd-kit/sortable";
+import { SortableItem } from './SortableItem';
+
 
 function App() {
+  const [languages, setLanguages] = useState(["JavaScript", "Python", "TypeScript"]);
+
+  const handleDragEnd = (event) => {
+    console.log('dragged')
+    const { active, over } = event
+    console.log('ACTIVE' + active.id)
+    console.log('OVER' + over.id)
+    if (active.id !== over.id) {
+      setLanguages((items) => {
+        const activeIndex = items.indexOf(active.id);
+        const overIndex = items.indexOf(over.id);
+        console.log(arrayMove(items, activeIndex, overIndex));
+        return arrayMove(items, activeIndex, overIndex);
+      });
+
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndContext
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+
+      <SortableContext
+        items={languages}
+        strategy={verticalListSortingStrategy}
+      >
+        {/* We need components that use the useSortable hook */}
+        {languages.map(language => <SortableItem key={language} id={language} />)}
+      </SortableContext>
+    </DndContext>
   );
 }
 
